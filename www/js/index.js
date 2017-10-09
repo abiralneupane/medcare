09f5380d6f472f916a05ffd97883260d0782e9e9
Abiral Neupane <abiral.neupane@gmail.com>
2017-10-09 20:19:07 +0545

    Comments added

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,23 @@
+/*
+ * app is main object and DB is database object. Any Database operation is done using DB. For example
+ * DB.searchAllMedicineByName(name, callback); Since database operation is asynchronous process, 
+ * it requires a callback function. All the functions and methods are wrapped by the object. Eg:
+ * 	var app = {
+ *		var1: '',
+ *		var2: 0,
+ *		method1: function(){},
+ *		method2: function(arg1){}
+ * 	};
+ *	
+ * And it will be called as app.var1 or app.method1(); or app.method2(arg);
+ * To make the back button work, we need to know where we are. So, the screens variable inside the app object
+ * stores the current screen and previous screen
+ * 
+ * To make the navigation faster, there is a buffer variable "categoryPage" and "productPage". If user have already viewed any pages
+ * instead of loading all the content from database again, it will load from the variable. It checks if some values are available in
+ * the variable. If not, only database's values are fetched. The condition would be of no use, if the application was dynamic.
+ */
+
 var app = {
     categoryPage: '',
 
@@ -16,7 +36,7 @@ var app = {
         this.bindEvents();
     },
 
-    // 'load', 'deviceready', 'offline', and 'online'.
+    /* Function that listen to the user event when the device is ready and when the back button is tapped */
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
         document.addEventListener("backbutton", this.sendBack, false);
@@ -44,15 +64,25 @@ var app = {
 
     onDeviceReady: function() {
         var self = app;
+        /* If app is not lauched for the first time. Only the loading screen is seen 
+         * It is done so that database can be setup for the first time 
+         */
+
         var applaunchCount = window.localStorage.getItem('launchCount');
         if(!applaunchCount){
             $('.loading').show();
         }
 
+        /* It will intialize the database, create it, and enter all the dummy data. 
+		 * The callback function written after DB.init runs after database and it's table are made ready
+         */
+
         DB.init(function(){
+            /* Hides loading button, and shows homepage */
             $('.loading').hide();
             app.home();
             
+            /* When user taps on search button */
             $('.btn-search').on('click', function(event){
                 event.preventDefault();
                 self.screens.prev = self.screens.current;
@@ -69,6 +99,9 @@ var app = {
                 var html = '<h3 class="subtitle">Search result for '+query+'</h3>';
 
                 if(typeof category == "undefined"){
+                
+                /* If user is in home page, the value for category is undefined, and the query will only search for name */
+                    
                     DB.searchAllMedicineByName(query, function(tx, results){
                         $('.screen').hide();
                         if(results.rows.length > 0){
@@ -87,6 +120,7 @@ var app = {
                         $('.screen').fadeIn(500);
                     });
                 }else{
+                    /* If user is in category page, the query will search for name in the current category */
                     DB.searchMedicineInCategory(query, category, function(tx, results){
                         if(results.rows.length > 0){
                             html += '<div class="medicine-container" data-category="'+category+'">';                    
@@ -141,6 +175,8 @@ var app = {
                 $('.content').html(html);
                 $('.screen').fadeIn(500);
 
+
+                /* When user clicks on category */
                 $(document).on('click','.category-list .category a', function(event){
                     event.preventDefault();
                     self.screens.prev = self.screens.current;
@@ -150,6 +186,8 @@ var app = {
                     $('.screen').hide();
 
                     self.tempObj = {id: id, title: title};
+                    
+                    /* List of medicines of the selected category are shown */
                     self.medicines(id, title);
                 });
             });
